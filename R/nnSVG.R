e2d1790d06c5b2d899762d8bca4cea7ad5aa1e91
Lukas Weber <lukas.weber.edu@gmail.com>
2022-02-18 17:26:25 -0500

    update filtering parameters, X argument, docs, examples, vignette

--- R/nnSVG.R
+++ R/nnSVG.R
@@ -1,78 +1,94 @@
 #' nnSVG
 #' 
-#' Run 'nnSVG' to identify spatially variable genes
+#' Function to run 'nnSVG' method to identify spatially variable genes (SVGs) in
+#' spatially resolved transcriptomics (ST) data.
 #' 
-#' Run 'nnSVG' method to identify spatially variable genes (SVGs) in spatially
-#' resolved transcriptomics data.
+#' Function to run 'nnSVG' method to identify spatially variable genes (SVGs) in
+#' spatially resolved transcriptomics (ST) data.
 #' 
-#' The method is based on nearest neighbor Gaussian processes (Datta et al.
-#' 2016) and uses the BRISC algorithm (Saha and Datta 2018). The method scales
-#' linearly in the number of spatial coordinates, and can be applied to datasets
-#' containing thousands of spatial coordinates.
+#' The 'nnSVG' method is based on nearest-neighbor Gaussian processes (Datta et
+#' al. 2016) and uses the BRISC algorithm (Saha and Datta 2018). The method
+#' scales linearly in the number of spatial locations, and can be applied to
+#' datasets containing thousands or more spatial locations. For more details on
+#' the method, see our paper.
 #' 
-#' This function runs BRISC once per gene for model fitting and parameter
-#' estimation, using parallelization for faster runtime with one core per BRISC
-#' run. The spatial covariance parameter estimates (sigma.sq, tau.sq, phi) from
-#' BRISC are stored in 'Theta' in the BRISC output.
+#' This function runs 'nnSVG' for a full dataset. Internally, the function calls
+#' BRISC once per gene to perform model fitting and parameter estimation, using
+#' parallelization with one core per BRISC run. The spatial covariance parameter
+#' estimates (sigma.sq, tau.sq, phi) are stored in 'Theta' in the BRISC output.
 #' 
-#' nnSVG performs inference on the 'sigma.sq' estimates using an approximate
-#' likelihood ratio test against a model without spatial terms, and uses these
-#' likelihood ratios to rank SVGs. We also calculate an effect size, defined as
-#' the proportion of spatial variance out of total variance, i.e. 'prop_sv =
+#' 'nnSVG' then performs inference on the 'sigma.sq' estimates using an
+#' approximate likelihood ratio (LR) test against a model without spatial terms,
+#' and uses the estimated LR statistics to rank SVGs. We also calculate an
+#' effect size, defined as the proportion of spatial variance, 'prop_sv =
 #' sigma.sq / (sigma.sq + tau.sq)'.
 #' 
-#' Likelihood ratio tests are calculated using the asymptotic chi-squared
-#' distribution with 2 degrees of freedom, and adjusted p-values using the
-#' Benjamini-Hochberg method.
+#' LR tests are calculated using the asymptotic chi-squared distribution with 2
+#' degrees of freedom. Multiple testing adjusted p-values are calculated using
+#' the Benjamini-Hochberg method.
 #' 
-#' Assumes the input is provided as a \code{\link{SpatialExperiment}} object
-#' with an \code{assay} slot containing either deviance residuals (e.g. from the
-#' \code{scry} package) or log-transformed normalized counts (e.g. from the
-#' \code{scran} package), and which has been filtered to remove low-quality
-#' spatial coordinates.
+#' The function assumes the input is provided as a \code{SpatialExperiment}
+#' object containing an \code{assay} slot containing either deviance residuals
+#' (e.g. from the \code{scry} package) or log-transformed normalized counts
+#' (e.g. from the \code{scran} package), which have been preprocessed, quality
+#' controlled, and filtered to remove any low-quality spatial locations.
 #' 
-#' Low-expressed genes can be filtered before providing the input to
-#' \code{nnSVG()}, or using the default filtering arguments in \code{nnSVG()}.
+#' Low-expressed genes can be filtered out either manually before providing the
+#' input to 'nnSVG', or using the filtering arguments and default values
+#' provided here.
 #' 
 #' 
-#' @param spe \code{SpatialExperiment} Input data, assumed to be a
-#'   \code{\link{SpatialExperiment}} object with \code{assay} slots containing
-#'   deviance residuals and/or log-transformed normalized counts, and spatial
-#'   coordinates stored in the \code{spatialCoords} slot.
+#' @param spe \code{SpatialExperiment}: Input data, assumed to be formatted as a
+#'   \code{SpatialExperiment} object with an \code{assay} slot containing either
+#'   deviance residuals (e.g. from the \code{scry} package) or log-transformed
+#'   normalized counts (e.g. from the \code{scran} package), and a
+#'   \code{spatialCoords} slot containing spatial coordinates of the
+#'   measurements.
 #' 
-#' @param x \code{numeric matrix} Optional matrix of covariates (e.g. known cell
-#'   types) per spatial coordinate. Number of rows must match the number of
-#'   spatial coordinates (columns) in the input object \code{spe}. Default =
-#'   NULL, which fits an intercept-only model.
+#' @param X \code{numeric matrix}: Optional design matrix containing columns of
+#'   covariates per spatial location, e.g. known spatial domains. Number of rows
+#'   must match the number of spatial locations. Default = NULL, which fits an
+#'   intercept-only model.
 #' 
-#' @param assay_name \code{character} Name of assay containing preprocessed
-#'   expression values to use for clustering, i.e. either deviance residuals or
-#'   log-transformed normalized counts. Assumed to be either
-#'   \code{binomial_deviance_residuals} or \code{logcounts}. Default =
+#' @param assay_name \code{character}: Name of the \code{assay} slot in the
+#'   input object containing the preprocessed gene expression values. For
+#'   example, \code{binomial_deviance_residuals} for deviance residuals from the
+#'   \code{scry} package, or \code{logcounts} for log-transformed normalized
+#'   counts from the \code{scran} package. Default =
 #'   \code{binomial_deviance_residuals}.
 #' 
-#' @param filter_genes \code{integer} Whether to filter low-expressed genes. If
-#'   a value is provided, genes with at least 1 unique molecular identifier
-#'   (UMI) count in at least this percentage of spatial coordinates will be
-#'   kept. Assumes \code{spe} contains an assay named \code{counts} containing
-#'   UMI counts. Default = 1, i.e. keep genes with at least 1 UMI in 1% of
-#'   spatial coordinates. Set to NULL to disable.
+#' @param filter_genes_ncounts \code{numeric}: Filtering parameter to filter
+#'   low-expressed genes. If values are provided, filtering will be performed to
+#'   retain genes containing at least \code{filter_genes_ncounts} expression
+#'   counts in at least \code{filter_genes_pcspots} percent of the total number
+#'   of spatial locations (spots). This also requires that the input object
+#'   contains an additional \code{assay} named \code{counts} containing raw gene
+#'   expression counts. Defaults: \code{filter_genes_ncounts} = 2,
+#'   \code{filter_genes_pcspots} = 0.5, i.e. keep genes with at least 2 counts
+#'   in at least 0.5% of spots. Set to NULL to disable filtering, e.g. if
+#'   filtering has already been performed separately.
 #' 
-#' @param filter_mito \code{logical} Whether to filter mitochondrial genes.
-#'   Assumes the \code{rowData} slot of \code{spe} contains a column named
-#'   \code{gene_name}, which can be used to identify mitochondrial genes.
-#'   Default = TRUE. Set to FALSE to disable.
+#' @param filter_genes_pcspots \code{numeric}: Second filtering parameter for
+#'   low-expressed genes. See \code{filter_genes_ncounts} for details.
 #' 
-#' @param n_threads \code{integer} Number of threads for parallelization.
+#' @param filter_mito \code{logical}: Whether to filter out mitochondrial genes,
+#'   identified by gene names starting with "MT" or "mt". This requires that the
+#'   \code{rowData} slot of the input object contains a column named
+#'   \code{gene_name}. Default = TRUE. Set to FALSE to disable, e.g. if
+#'   mitochondrial genes are of interest or have already been filtered out from
+#'   the input object.
+#' 
+#' @param n_threads \code{integer}: Number of threads for parallelization.
 #'   Default = 1.
 #' 
-#' @param verbose \code{logical} Whether to display verbose output from
-#'   \code{BRISC}. Default = FALSE.
+#' @param verbose \code{logical}: Whether to display verbose output for model
+#'   fitting and parameter estimation from \code{BRISC}. Default = FALSE.
 #' 
 #' 
-#' @return Returns output values (including parameter estimates, likelihood
-#'   ratios, and adjusted p-values) in \code{rowData} in the \code{spe}
-#'   \code{SpatialExperiment} object.
+#' @return Returns output values as additional columns in the \code{rowData}
+#'   slot of the input object, including spatial covariance parameter estimates,
+#'   likelihood ratio (LR) statistics, effect sizes (proportion of spatial
+#'   covariance), p-values, and multiple testing adjusted p-values.
 #' 
 #' 
 #' @importFrom SpatialExperiment spatialCoords
@@ -99,18 +115,7 @@
 #' # keep only spots over tissue
 #' spe <- spe[, colData(spe)$in_tissue == 1]
 #' 
-#' # filter low-expressed genes
-#' filter_genes <- 1
-#' n_spots <- ceiling(filter_genes / 100 * ncol(spe))
-#' ix_remove <- rowSums(counts(spe) > 0) < n_spots
-#' spe <- spe[!ix_remove, ]
-#' 
-#' # filter mitochondrial genes
-#' is_mito <- grepl("(^MT-)|(^mt-)", rowData(spe)$gene_name)
-#' spe <- spe[!is_mito, ]
-#' 
 #' # calculate deviance residuals using scry package
-#' set.seed(123)
 #' spe <- nullResiduals(spe, assay = "counts", 
 #'                      fam = "binomial", type = "deviance")
 #' 
@@ -119,38 +124,48 @@
 #' spe <- spe[sample(seq_len(6)), ]
 #' 
 #' # run nnSVG
-#' # note: gene filtering was already performed above, so disable it here
+#' # set seed for reproducibility
 #' set.seed(123)
-#' spe <- nnSVG(spe, filter_genes = FALSE, filter_mito = FALSE, n_threads = 1)
+#' spe <- nnSVG(spe)
 #' 
 #' # show results
 #' # for more details see extended example in vignette
 #' rowData(spe)
 #' 
-nnSVG <- function(spe, x = NULL, 
-                  assay_name = c("binomial_deviance_residuals", "logcounts"), 
-                  filter_genes = 1, filter_mito = TRUE, 
+nnSVG <- function(spe, X = NULL, 
+                  assay_name = "binomial_deviance_residuals", 
+                  filter_genes_ncounts = 2, filter_genes_pcspots = 0.5, 
+                  filter_mito = TRUE, 
                   n_threads = 1, verbose = FALSE) {
   
-  if (!is.null(x)) stopifnot(nrow(x) == ncol(spe))
+  if (!is.null(X)) stopifnot(nrow(X) == ncol(spe))
   
-  assay_name <- match.arg(assay_name, c("binomial_deviance_residuals", "logcounts"))
   stopifnot(assay_name %in% assayNames(spe))
   
   # --------------
   # gene filtering
   # --------------
   
-  if (!is.null(filter_genes) & filter_genes > 0) {
+  # low-expressed genes
+  if (!is.null(filter_genes_ncounts) | !is.null(filter_genes_pcspots)) {
+    message("Gene filtering: retaining genes with at least ", 
+            filter_genes_ncounts, " counts in at least " , 
+            filter_genes_pcspots, "% of spatial locations")
     stopifnot("counts" %in% assayNames(spe))
-    n_spots <- ceiling(filter_genes / 100 * ncol(spe))
-    ix_remove <- rowSums(counts(spe) > 0) < n_spots
-    message("removing ", sum(ix_remove), " out of ", nrow(spe), " genes due to low expression")
+    
+    nspots <- ceiling(filter_genes_pcspots / 100 * ncol(spe))
+    ix_remove <- rowSums(counts(spe) >= filter_genes_ncounts) < nspots
+    message("removed ", sum(ix_remove), " out of ", nrow(spe), 
+            "genes due to low expression")
+    
     spe <- spe[!ix_remove, ]
   }
   
+  # mitochondrial genes
   if (filter_mito) {
+    message("Gene filtering: removing mitochondrial genes")
     stopifnot("gene_name" %in% colnames(rowData(spe)))
+    
     is_mito <- grepl("(^MT-)|(^mt-)", rowData(spe)$gene_name)
     message("removing ", sum(is_mito), " mitochondrial genes")
     spe <- spe[!is_mito, ]
@@ -161,7 +176,7 @@ nnSVG <- function(spe, x = NULL,
   # ---------
   
   y <- assays(spe)[[assay_name]]
-
+  
   # scale coordinates proportionally
   coords <- spatialCoords(spe)
   range_all <- max(apply(coords, 2, function(col) diff(range(col))))
@@ -182,7 +197,7 @@ nnSVG <- function(spe, x = NULL,
     y_i <- y[i, ]
     suppressWarnings({
       runtime <- system.time({
-        out_i <- BRISC_estimation(coords = coords, y = y_i, x = x, 
+        out_i <- BRISC_estimation(coords = coords, y = y_i, x = X, 
                                   cov.model = "exponential", 
                                   ordering = order_brisc, neighbor = nn_brisc, 
                                   verbose = verbose)
@@ -223,18 +238,9 @@ nnSVG <- function(spe, x = NULL,
   } else {
     # return NAs if logcounts not provided
     mat_brisc <- cbind(
-      mat_brisc, 
-      mean = NA, 
-      var = NA, 
-      spcov = NA
+      mat_brisc, mean = NA, var = NA, spcov = NA
     )
   }
-  
-  # ratio of spatial to non-spatial variance
-  mat_brisc <- cbind(
-    mat_brisc, 
-    ratio_sv = mat_brisc[, "sigma.sq"] / mat_brisc[, "tau.sq"]
-  )
   
   # proportion of spatial variance out of total variance
   mat_brisc <- cbind(
@@ -250,10 +256,10 @@ nnSVG <- function(spe, x = NULL,
   
   loglik_lm <- sapply(seq_len(nrow(spe)), function(i) {
     y_i <- y[i, ]
-    if (is.null(x)) {
-      x <- rep(1, ncol(spe))
+    if (is.null(X)) {
+      X <- rep(1, ncol(spe))
     }
-    as.numeric(logLik(lm(y_i ~ x)))
+    as.numeric(logLik(lm(y_i ~ X)))
   })
   
   mat_brisc <- cbind(
@@ -261,7 +267,7 @@ nnSVG <- function(spe, x = NULL,
     loglik_lm = loglik_lm
   )
   
-  # calculate LR statistics and tests (Wilks' theorem, asymptotic chi-square
+  # calculate LR statistics and tests (Wilks' theorem, asymptotic chi-square 
   # with 2 degrees of freedom since 2 more parameters in full model)
   
   LR_stat = -2 * (mat_brisc[, "loglik_lm"] - mat_brisc[, "loglik"])
@@ -270,7 +276,6 @@ nnSVG <- function(spe, x = NULL,
   padj <- p.adjust(pval, method = "BH")
   
   # rank SVGs according to LR statistics
-  
   LR_rank <- rank(-1 * LR_stat)
   
   mat_brisc <- cbind(
